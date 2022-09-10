6d8c6085756de3c071216cb4cacdfcfd372324f6
sam <coblesam360@gmail.com>
2022-09-10 00:09:26 -0700

    function to fetch available seats of course

--- App.js
+++ App.js
@@ -1,21 +1,93 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+
+import React, {
+	Component,
+	useState,
+	useEffect,
+} from 'react';
+
+import {
+	StyleSheet,
+	Text,
+	View,
+	Button,
+} from 'react-native';
 
 export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Welcome to UBC Course Sleuth!!!</Text>
-      <Text> ( name in progress :) ) </Text>
-      <StatusBar style="auto" />
-    </View>
-  );
+
+	// initial set to -1 as n/a
+	const [seatsRemaining, setSeatsRemaining] = useState(-1)
+
+	async function getCourseStatus() {
+		try {
+			// ling 170 001 course info page for example
+			// const url = 'https://courses.students.ubc.ca/cs/courseschedule?pname=subjarea&tname=subj-section&dept=LING&course=170&section=001';
+			const deptCode = 'LING';
+			const courseCode = '170';
+			const sectionCode = '001';
+			const url = 
+				'https://courses.students.ubc.ca/cs/courseschedule?pname=subjarea&tname=subj-section&dept=' +
+				deptCode +
+				'&course=' +
+				courseCode +
+				'&section=' +
+				sectionCode;
+
+			// get html content from url
+			const res = await fetch(url);
+			// convert html response to plain text
+			const text = await res.text();
+
+			// console.log(url, text.split('\n')[30]);
+
+			// use regex to isolate remaining seats
+			const generalSeatRE = /General Seats Remaining:.*<strong>\d+</
+			const matches = text.match(generalSeatRE);
+			if (matches.length == 0)
+				throw 'bad site';
+			if (matches.length != 1) 
+				throw "too many matches";
+
+			const numberRE = />\d+</;
+			const numberMatches = matches[0].match(numberRE);
+			const match = numberMatches[numberMatches.length - 1];
+			// last character of the string
+			const remainingGeneralSeats = parseInt(match.slice(1,-1));
+
+
+			// console.log('matches', numberMatches);
+
+
+			setSeatsRemaining(remainingGeneralSeats);
+
+		} catch (e) {
+			console.log(e);
+		}
+	}
+
+
+	useEffect(() => {
+		// code to run on mount
+		getCourseStatus();
+	}, []);
+
+	return (
+		<View style={styles.container}>
+			<Text>Welcome to UBC Course Sleuth!!!</Text>
+			<Text> ( name in progress :) ) </Text>
+			<Button onPress={getCourseStatus} title="Update status of Ling 170 001" />
+			<Text> There are {seatsRemaining} seats remaining! </Text>
+			<StatusBar style="auto" />
+		</View>
+	);
+
 }
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
+	container: {
+		flex: 1,
+		backgroundColor: '#fff',
+		alignItems: 'center',
+		justifyContent: 'center',
+	},
 });
