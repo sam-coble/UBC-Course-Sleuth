9886896fb45541976e05383f39cfb03c17dbd9e1
sam <coblesam360@gmail.com>
2022-09-10 23:55:35 -0700

    registration for push notifications

--- App.js
+++ App.js
@@ -1,5 +1,6 @@
 import { StatusBar } from 'expo-status-bar';
-
+import * as Device from 'expo-device';
+import * as Notifications from 'expo-notifications';
 import React, {
 	Component,
 	useState,
@@ -13,10 +14,28 @@ import {
 	Button,
 } from 'react-native';
 
+
+
+Notifications.setNotificationHandler({
+  handleNotification: async () => ({
+    shouldShowAlert: true,
+    shouldPlaySound: false,
+    shouldSetBadge: false,
+  }),
+});
+
 export default function App() {
 
 	// initial set to -1 as n/a
-	const [seatsRemaining, setSeatsRemaining] = useState(-1)
+	const [seatsRemaining, setSeatsRemaining] = useState(-1);
+
+	// notification token
+	const [expoPushToken, setExpoPushToken] = useState('dne');
+
+	useEffect(() => {
+		registerForPushNotificationsAsync().then(token => setExpoPushToken(token));
+
+	}, []);
 
 	async function getCourseStatus() {
 		try {
@@ -69,6 +88,11 @@ export default function App() {
 	useEffect(() => {
 		// code to run on mount
 		getCourseStatus();
+
+		return () => {
+			// code to run on cleanup
+
+		}
 	}, []);
 
 	return (
@@ -77,6 +101,7 @@ export default function App() {
 			<Text> ( name in progress :) ) </Text>
 			<Button onPress={getCourseStatus} title="Update status of Ling 170 001" />
 			<Text> There are {seatsRemaining} seats remaining! </Text>
+			<Text> Notification token: {expoPushToken}</Text>
 			<StatusBar style="auto" />
 		</View>
 	);
@@ -91,3 +116,35 @@ const styles = StyleSheet.create({
 		justifyContent: 'center',
 	},
 });
+
+
+async function registerForPushNotificationsAsync() {
+  let token;
+  if (Device.isDevice) {
+    const { status: existingStatus } = await Notifications.getPermissionsAsync();
+    let finalStatus = existingStatus;
+    if (existingStatus !== 'granted') {
+      const { status } = await Notifications.requestPermissionsAsync();
+      finalStatus = status;
+    }
+    if (finalStatus !== 'granted') {
+      alert('Failed to get push token for push notification!');
+      return;
+    }
+    token = (await Notifications.getExpoPushTokenAsync()).data;
+    console.log(token);
+  } else {
+    alert('Must use physical device for Push Notifications');
+  }
+
+  if (Platform.OS === 'android') {
+    Notifications.setNotificationChannelAsync('default', {
+      name: 'default',
+      importance: Notifications.AndroidImportance.MAX,
+      vibrationPattern: [0, 250, 250, 250],
+      lightColor: '#FF231F7C',
+    });
+  }
+
+  return token;
+}
