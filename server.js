641eb17ab80044b6bdf69642609aaef13f99d19e
sam <coblesam360@gmail.com>
2022-09-11 20:55:18 -0700

    spaces -> tabs

--- server.js
+++ server.js
@@ -12,21 +12,21 @@ const somePushTokens = readFileSync('tokens.txt', {encoding:'utf8', flag:'r'}).s
 // Create the messages that you want to send to clients
 let messages = [];
 for (let pushToken of somePushTokens) {
-  // Each push token looks like ExponentPushToken[xxxxxxxxxxxxxxxxxxxxxx]
+	// Each push token looks like ExponentPushToken[xxxxxxxxxxxxxxxxxxxxxx]
 
-  // Check that all your push tokens appear to be valid Expo push tokens
-  if (!Expo.isExpoPushToken(pushToken)) {
-    console.error(`Push token ${pushToken} is not a valid Expo push token`);
-    continue;
-  }
+	// Check that all your push tokens appear to be valid Expo push tokens
+	if (!Expo.isExpoPushToken(pushToken)) {
+		console.error(`Push token ${pushToken} is not a valid Expo push token`);
+		continue;
+	}
 
-  // Construct a message (see https://docs.expo.io/push-notifications/sending-notifications/)
-  messages.push({
-    to: pushToken,
-    sound: 'default',
-    body: 'This is a test notification',
-    data: { withSome: 'data' },
-  })
+	// Construct a message (see https://docs.expo.io/push-notifications/sending-notifications/)
+	messages.push({
+		to: pushToken,
+		sound: 'default',
+		body: 'This is a test notification',
+		data: { withSome: 'data' },
+	})
 }
 
 // The Expo push notification service accepts batches of notifications so
@@ -37,22 +37,22 @@ for (let pushToken of somePushTokens) {
 let chunks = expo.chunkPushNotifications(messages);
 let tickets = [];
 (async () => {
-  // Send the chunks to the Expo push notification service. There are
-  // different strategies you could use. A simple one is to send one chunk at a
-  // time, which nicely spreads the load out over time:
-  for (let chunk of chunks) {
-    try {
-      let ticketChunk = await expo.sendPushNotificationsAsync(chunk);
-      console.log(ticketChunk);
-      tickets.push(...ticketChunk);
-      // NOTE: If a ticket contains an error code in ticket.details.error, you
-      // must handle it appropriately. The error codes are listed in the Expo
-      // documentation:
-      // https://docs.expo.io/push-notifications/sending-notifications/#individual-errors
-    } catch (error) {
-      console.error(error);
-    }
-  }
+	// Send the chunks to the Expo push notification service. There are
+	// different strategies you could use. A simple one is to send one chunk at a
+	// time, which nicely spreads the load out over time:
+	for (let chunk of chunks) {
+		try {
+			let ticketChunk = await expo.sendPushNotificationsAsync(chunk);
+			console.log(ticketChunk);
+			tickets.push(...ticketChunk);
+			// NOTE: If a ticket contains an error code in ticket.details.error, you
+			// must handle it appropriately. The error codes are listed in the Expo
+			// documentation:
+			// https://docs.expo.io/push-notifications/sending-notifications/#individual-errors
+		} catch (error) {
+			console.error(error);
+		}
+	}
 })();
 
 
