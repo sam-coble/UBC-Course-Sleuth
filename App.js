1e32ae3fdc9e43b53693d1525bf96d3e88cd541a
czhao1125 <crystalz151012@gmail.com>
2022-09-12 18:58:42 -0700

    Cleaned up the code

--- App.js
+++ App.js
@@ -40,26 +40,6 @@ const Tab = createBottomTabNavigator();
 const Stack = createNativeStackNavigator();
 
 
-// class HomeScreen extends React.Component {  
-// 	render() {  
-// 	  return (  
-// 		  <View style={styles.container}>  
-// 			<Text>Home Screen</Text>  
-// 		  </View>  
-// 	  );  
-// 	}  
-//   }  
-
-// class SearchScreen extends React.Component {
-// 	render() {
-// 		return (
-// 			<View style={styles.container}>  
-//           		<Text>Search Screen</Text>  
-//         	</View>  
-// 		);
-// 	}
-// }
-
  class SettingScreen extends React.Component {  
      render() {  
          return (  
@@ -69,50 +49,6 @@ const Stack = createNativeStackNavigator();
          );  
      }  
  }  
-
-// const TabNavigator = createMaterialBottomTabNavigator(  
-//     {  
-//         Home: { screen: HomeScreen,  
-//             navigationOptions:{  
-//                 tabBarLabel:'Home',  
-//                 tabBarIcon: ({ tintColor }) => (  
-//                     <View>  
-//                         <Icon style={[{color: tintColor}]} size={25} name={'ios-home'}/>  
-//                     </View>),  
-//             }  
-//         },  
-//         Profile: { screen: SearchScreen,  
-//             navigationOptions:{  
-//                 tabBarLabel:'Search',  
-//                 tabBarIcon: ({ tintColor }) => (  
-//                     <View>  
-//                         <Icon style={[{color: tintColor}]} size={25} name={'ios-person'}/>  
-//                     </View>),  
-//                 activeColor: '#f60c0d',  
-//                 inactiveColor: '#f65a22',  
-//                 barStyle: { backgroundColor: '#f69b31' },  
-//             }  
-//         },  
-//         Image: { screen: SettingScreen,  
-//             navigationOptions:{  
-//                 tabBarLabel:'Setting',  
-//                 tabBarIcon: ({ tintColor }) => (  
-//                     <View>  
-//                         <Icon style={[{color: tintColor}]} size={25} name={'ios-images'}/>  
-//                     </View>),  
-//                 activeColor: '#615af6',  
-//                 inactiveColor: '#46f6d7',  
-//                 barStyle: { backgroundColor: '#67baf6' },  
-//             }  
-//         },   
-//     },  
-//     {  
-//       initialRouteName: "Home",  
-//       activeColor: '#f0edf6',  
-//       inactiveColor: '#226557',  
-//       barStyle: { backgroundColor: '#3BAD87' },  
-//     },  
-// );  
 
 export default function App() {
 
@@ -196,6 +132,8 @@ export default function App() {
 		//	<Text> Notification token: {expoPushToken}</Text>
 		//	<StatusBar style="auto" />
 		//</View>
+
+		// Bottom Tabs
 		<NavigationContainer>
 			<Tab.Navigator
         	screenOptions={({ route }) => ({
