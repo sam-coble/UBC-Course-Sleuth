3f74fa7000bdb5ff5300d37e59a600e876d4762d
sam <coblesam360@gmail.com>
2022-09-12 22:00:18 -0700

    search screen start

--- screens/SearchScreen.js
+++ screens/SearchScreen.js
@@ -6,6 +6,7 @@ import { createStackNavigator } from "@react-navigation/stack";
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
 import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
 import Ionicons from 'react-native-vector-icons/Ionicons';
+import {SearchBar} from 'react-native-elements';
 import React, {
 	Component,
 	useState,
@@ -17,15 +18,37 @@ import {
 	Text,
 	View,
 	Button,
+	FlatList,
 } from 'react-native';
 
 
-function SearchScreen(props) {
+export default function SearchScreen(props) {
 	return (
-		<View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
-			<Text>Search Screen</Text>
+		<View style={styles.viewStyles}>
+			<SearchBar 
+				round
+				searchIcon={{ size: 24 }}
+				onChangeText={text => this.SearchFilterFunction(text)}
+				onClear={text => this.SearchFilterFunction('')}
+				placeholder="Type Here..."
+				value={this.state.search} 
+			/>
+			<FlatList
+				data={this.state.dataSource}
+				ItemSeparatorComponent={this.ListViewItemSeparator}
+				//Item Separator View
+				renderItem={({ item }) => (
+				// Single Comes here which will be repeatative for the FlatListItems
+				<Text style={styles.textStyle}>{item.title}</Text>
+				)}
+				enableEmptySections={true}
+				style={{ marginTop: 10 }}
+				keyExtractor={(item, index) => index.toString()}
+	        />
+
 		</View>
 	);
 }
 
-export default SearchScreen;
+
+
