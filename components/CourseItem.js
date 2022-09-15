55178b8c3840a3ea86421e3275e9666353229afc
sam <coblesam360@gmail.com>
2022-09-14 23:57:10 -0700

    Course Item Component

---
+++ components/CourseItem.js
@@ -0,0 +1,49 @@
+import React, {
+	Component,
+	useState,
+	useEffect,
+} from 'react';
+
+import {
+	StyleSheet,
+	Text,
+	View,
+} from 'react-native';
+
+export default function CourseItem(props) {
+	
+	const [courseInfo, setCourseInfo] = useState({});
+
+	useEffect(() => {
+		// fetch course info?
+
+
+		return () => {
+
+		}
+	}, []);
+
+	return (
+		<View style={styles.view}>
+			<Text>
+				{`${courseInfo.dept} ${courseInfo.courseCode}: ${courseInfo.title}`}
+			</Text>
+			<View style={styles.starContainer}>
+				{
+					return new Array(courseInfo.starcont).map(e => {
+						return <Text>*</Text>
+					})
+				}
+			</View>
+		</View>
+	)
+}
+
+const styles = StyleSheet.create({
+	'view': {
+
+	},
+	'starContainer': {
+
+	},
+});
